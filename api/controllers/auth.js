4052ff771ab700fe14d897510e714fd6e00337e1
Nilupul Madhawa <68707780+nilupulmadhawa@users.noreply.github.com>
2023-10-03 23:33:42 +0530

    update

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -4,39 +4,42 @@ import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
     //CHECK EXISTING USER
-    try {
-        const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+    const q = "SELECT * FROM users WHERE email = '" + req.body.email + "' OR username = '" + req.body.username + "'";
 
-        db.query(q, [req.body.email, req.body.username], (err, data) => {
+    db.query(q, (err, data) => {
+        if (err) return res.status(500).json(err);
+        if (data.length) return res.status(409).json("User already exists!");
+
+        //Hash the password and create a user
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+        const q = "INSERT INTO users(`username`,`email`,`password`) VALUES ('" + req.body.username + "','" + req.body.email + "','" + hash + "')";
+
+        db.query(q, (err, data) => {
             if (err) return res.status(500).json(err);
-            if (data.length) return res.status(409).json("User already exists!");
-
-            //Hash the password and create a user
-            const salt = bcrypt.genSaltSync(10);
-            const hash = bcrypt.hashSync(req.body.password, salt);
-
-            const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
-            const values = [req.body.username, req.body.email, hash];
-
-            db.query(q, [values], (err, data) => {
-                if (err) return res.status(500).json(err);
-                return res.status(200).json("User has been created.");
-            });
+            return res.status(200).json("User has been created.");
         });
-    } catch (error) {
-        return res.json("Something went wrong!");
-    }
+    });
 };
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
     //CHECK USER
 
+    const q = "SELECT * FROM users WHERE username = '" + req.body.username + "'";
     try {
+        const secret = process.env.RECAPTCHA_SECRET_KEY;
+        const token = req.body.reToken;
+        const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`;
+        const response = await fetch(verificationUrl).then(res => res.json());
+        // RECAPTCHA validation
+        if (response.success) {
+            console.log("success");
+        } else {
+            return res.status(404).json('Please verify that you are not a robot');
+        }
 
-
-        const q = "SELECT * FROM users WHERE username = ?";
-
-        db.query(q, [req.body.username], (err, data) => {
+        db.query(q, (err, data) => {
             if (err) return res.status(500).json(err);
             if (data.length === 0) return res.status(404).json("User not found!");
 
@@ -53,25 +56,12 @@ export const login = (req, res) => {
             const { password, ...other } = data[0];
 
             res
-                .cookie("access_token", token, {
-                    httpOnly: true,
-                })
+                .cookie("access_token", token)
                 .status(200)
                 .json(other);
         });
-    } catch (error) {
-        return res.json("Something went wrong!");
-    }
-};
+    };
 
-export const logout = (req, res) => {
-    try {
-
-        res.clearCookie("access_token", {
-            sameSite: "none",
-            secure: true
-        }).status(200).json("User has been logged out.")
-    } catch (error) {
-        return res.json("Something went wrong!");
-    }
-};
+    export const logout = (req, res) => {
+        res.clearCookie("access_token").status(200).json("User has been logged out.")
+    };
